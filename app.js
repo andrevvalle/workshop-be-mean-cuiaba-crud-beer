3bbaf5029061ffd589e9463db5e730f58d1391bb
Suissa <jnascimento@gmail.com>
2014-08-15 20:41:15 -0400

    Subindo arquivos

---
+++ app.js
@@ -0,0 +1,74 @@
+
+/**
+ * Module dependencies
+ */
+
+var express = require('express'),
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override'),
+  errorHandler = require('errorhandler'),
+  morgan = require('morgan'),
+  routes = require('./routes'),
+  partials = require('./routes/partials'),
+  expose = require('./routes/expose'),
+  api = require('./routes/api'),
+  apiBeer = require('./routes/api/beers'),
+  http = require('http'),
+  path = require('path');
+
+var app = module.exports = express();
+
+/**
+ * Configuration
+ */
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+
+var env = process.env.NODE_ENV || 'development';
+
+// development only
+if (env === 'development') {
+  app.use(errorHandler());
+}
+
+// production only
+if (env === 'production') {
+  // TODO
+}
+
+
+/**
+ * Routes
+ */
+
+// serve index  
+app.use('/', routes);
+
+// server view partials
+app.use('/partials', partials);
+app.use('/expose', expose);
+
+// JSON API
+app.use('/api', api);
+app.use('/api/beers', apiBeer);
+
+// redirect all others to the index (HTML5 history)
+app.get('*', function(req, res, next) {
+  res.render('index');
+});
+
+/**
+ * Start Server
+ */
+
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ public/js/controllers.js
@@ -0,0 +1,63 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', []).
+  controller('AppCtrl', function ($scope, $http) {
+
+    $http({
+      method: 'GET',
+      url: '/api/name'
+    }).
+    success(function (data, status, headers, config) {
+      $scope.name = data.name;
+    }).
+    error(function (data, status, headers, config) {
+      $scope.name = 'Error!';
+    });
+
+  }).
+  controller('BeerListController', ['$scope', '$http',
+    function ($scope, $http) {
+
+    $http({
+      method: 'GET',
+      url: '/api/beers'
+    }).
+    success(function (data, status, headers, config) {
+      $scope.beers = data;
+      $scope.msg = 'Listagem completa!';
+    }).
+    error(function (data, status, headers, config) {
+      $scope.msg = 'Error!';
+    });
+
+  }]).
+  controller('BeerCreateController', ['$scope', '$http',
+    function ($scope, $http) {
+    $scope.msg = '';
+
+    $scope.create = function(entity){
+      $http({
+        method: 'POST',
+        url: '/api/beers',
+        data: entity
+      }).
+      success(function (data, status, headers, config) {
+        $scope.beer = data;
+        $scope.msg = 'Cadastro da cerveja '+data.name+ ' completa!';
+      }).
+      error(function (data, status, headers, config) {
+        $scope.msg = 'Error!';
+      });
+    }
+
+  }]).
+  controller('MyCtrl1', function ($scope) {
+    // write Ctrl here
+
+  }).
+  controller('MyCtrl2', function ($scope) {
+    // write Ctrl here
+
+  });
